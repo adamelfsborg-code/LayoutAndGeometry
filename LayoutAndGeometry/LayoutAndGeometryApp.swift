82b17378ace7c3bd7edc97d88524f66067c81193
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-27 07:57:43 +0200

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Adam Elfsborg on 2024-08-27.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
