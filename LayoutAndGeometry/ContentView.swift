2a0fc771e9530f8a286e3bec8e31a73dae697296
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-27 10:59:35 +0200

    feat: challenge

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,25 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { number in
-                    Text("Number \(number)")
-                        .font(.largeTitle)
-                        .padding()
-                        .background(.red)
-                        .frame(width: 200, height: 200)
-                        .visualEffect { content, proxy in
-                            content
-                                .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX / 8),
-                                                  axis: (x: 0, y: 1, z: 0))
-                        }
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<500) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                Color(
+                                    hue: min(1.0, proxy.frame(in: .global).minY / fullView.size.height),
+                                    saturation: min(1.0, proxy.frame(in: .global).width / fullView.size.height),
+                                    brightness: min(1.0, proxy.frame(in: .global).midY / fullView.size.height)
+                                )
+                            )
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5,
+                                              axis: (x: 0, y: 1, z: 0))
+                            .opacity(min(max(Double(((proxy.frame(in: .global).minY - 50) / (150 - 50))), 0), 1))
+                            .scaleEffect(max(0.5, (proxy.frame(in: .global).minY - 0) / (fullView.size.height - 0)))
+                    }
+                    .frame(height: 40)
                 }
             }
-            .scrollTargetLayout()
         }
-        .scrollTargetBehavior(.viewAligned)
     }
 }
 
