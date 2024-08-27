03c2930bf82e4d6fd0b5f9113296fcdbafefbef7
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-27 09:24:36 +0200

    feat: fun but hard geometry layouts

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,22 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    let colors: [Color] = [.red, .green, .yellow, .blue, .purple, .pink, .orange]
     var body: some View {
-        GeometryReader { fullView in
-            ScrollView {
-                ForEach(0..<50) { index in
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { number in
                     GeometryReader { proxy in
-                        Text("Row #\(index)")
-                            .font(.title)
-                            .frame(maxWidth: .infinity)
-                            .background(colors[index % 7])
+                        Text("Number \(number)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
                             .rotation3DEffect(
-                                .degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5,
+                                .degrees(-proxy.frame(in: .global).minX / 8),
                                 axis: (x: 0, y: 1, z: 0)
                             )
+                            .frame(width: 200, height: 200)
                     }
-                    .frame(height: 40)
+                    .frame(width: 200, height: 200)
                 }
             }
         }
